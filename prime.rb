44e46f93d44cdca338836fac198b81f2f5ba936d
Christine Cua <cloudycua@gmail.com>
2018-02-26 20:13:33 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime(integer)
   prime_numbers = []
   i = 0
-  while 2 ** i - 1 < integer.to_i
-    prime_numbers << 2 ** i - 1
+  while (2 ** i - 1) < integer.to_i
+    prime_numbers << (2 ** i - 1)
     i += 1
   end
   if prime_numbers.include?(integer)
