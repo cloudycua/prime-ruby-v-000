e6d24af2e58680096d828a606409d0d55bd868af
Christine Cua <cloudycua@gmail.com>
2018-02-26 20:02:34 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime(integer)
+  prime_numbers = []
+  i = 2
+  while (2 ** i) - 2 < integer
+    prime_numbers << (2 ** i) - 1
+    i += 1
+  end
+  if prime_numbers.include?(integer)
+    true
+  else
+    false
+  end
+end
+
