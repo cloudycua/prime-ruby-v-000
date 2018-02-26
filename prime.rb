0f53387ed79754fbd8f7868f0c9e23e63b00f1a1
Christine Cua <cloudycua@gmail.com>
2018-02-26 21:04:37 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,3 +1,14 @@
 def prime?(number)
-  (2..n/2).none?{|i| number % i == 0}
+  if number == 2 || number == 1
+    true
+  elsif number <=0 || number.even?
+    false
+  else
+    divisors = (2..number/2).to_a
+    if number.any? {|i| number % i == 0}
+      true
+    else
+      false
+    end
+  end
 end
