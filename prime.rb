ecf8c06ed0b3111b026a6ff1cb89888758118a68
Christine Cua <cloudycua@gmail.com>
2018-02-26 20:37:48 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,16 @@
 def prime?(number)
-  i = 1
-  while i < number
-    if number % i == 0
-      false
-    else
-      i += 1
+  if number <= 0
+    false
+  elsif number.even? && number > 2
+    false
+  else
+    i = 1
+    while i < number
+      if number % i == 0
+        false
+      else
+        i += 1
+      end
     end
   end
 end
